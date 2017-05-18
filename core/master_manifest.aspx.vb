c66341b79ab6d2d527a8ee049c64129e436418e8
Admin D@redbean <samuel.surya@gmail.com>
2017-05-18 10:15:49 +0700

    correction manifest

--- core/master_manifest.aspx.vb
+++ core/master_manifest.aspx.vb
@@ -1,7 +1,7 @@
 ﻿
 Partial Class OPH_welcome_default_master_manifest
     Inherits System.Web.UI.Page
-    Dim contentofCacheManifest As String
+    Public contentofCacheManifest As String
 
     Private Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
         Response.Cache.SetCacheability(HttpCacheability.NoCache)
