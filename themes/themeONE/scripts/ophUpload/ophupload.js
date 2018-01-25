4070ef4325c4ebf135641e1bb2940ef1cf0f4d21
Administrator@LIBERTYJAYAONE.LOCAL <Administrator@LIBERTYJAYAONE.LOCAL>
2018-01-25 09:16:14 +0700

    update themes

---
+++ themes/themeONE/scripts/ophUpload/ophupload.js
@@ -0,0 +1,49 @@
+
+//upload class
+var Upload = function (file) {
+    this.file = file;
+};
+
+Upload.prototype.getType = function () {
+    return this.file.type;
+};
+Upload.prototype.getSize = function () {
+    return this.file.size;
+}; 
+Upload.prototype.getName = function () {
+    return this.file.name;
+};
+Upload.prototype.doUpload = function (url, successF, errorF) {
+    var that = this;
+    var formData = new FormData();
+
+    // add assoc key values, this will be posts values
+    formData.append("file", this.file, this.getName());
+    formData.append("upload_file", true);
+
+    $.ajax({
+        type: "POST",
+        url: url,
+        xhr: function () {
+            var myXhr = $.ajaxSettings.xhr();
+            if (myXhr.upload) {
+                myXhr.upload.addEventListener('progress', that.progressHandling, false);
+            }
+            return myXhr;
+        },
+        success: function (data) {
+            if (typeof successF == "function") successF(data);
+            // your callback here
+        },
+        error: function (error) {
+            if (typeof errorF == "function") errorF(error);
+            // handle error
+        },
+        async: true,
+        data: formData,
+        cache: false,
+        contentType: false,
+        processData: false,
+        timeout: 60000
+    });
+}
