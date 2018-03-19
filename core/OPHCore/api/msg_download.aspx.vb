7e36a289491271058843cfe7bd2327889fcb1d5d
Administrator <Administrator@REDBEAN>
2018-03-19 15:32:09 +0700

    update download

--- core/OPHCore/api/msg_download.aspx.vb
+++ core/OPHCore/api/msg_download.aspx.vb
@@ -16,16 +16,23 @@ Partial Class OPHCore_api_msg_download
         Dim fieldAttachment As String = getQueryVar("fieldAttachment")
 
         'Dim fieldKey As String = getQueryVar("fieldKey")
-        Dim ocode = Left(code, 1) & "a" & code.Substring(2, Len(code) - 2)
-        Dim acctdbse = "select accountDBGUID from modl where ModuleId='" & code & "'"
-        acctdbse = runSQLwithResult(acctdbse, curODBC)
-        Dim con = "select odbc from acctdbse where accountDBGUID ='" & acctdbse & "'"
-        con = runSQLwithResult(con, contentOfsequoiaCon)
 
+        Dim ocode = code 'Left(code, 1) & "a" & code.Substring(2, Len(code) - 2)
+        'remarked andre
+        'Dim acctdbse = "select accountDBGUID from modl where ModuleId='" & code & "'"
+        'acctdbse = runSQLwithResult(acctdbse, curODBC)
+        'Dim con = "select odbc from acctdbse where accountDBGUID ='" & acctdbse & "'"
+        'con = runSQLwithResult(con, contentOfsequoiaCon)
+
+        'added andre
+        Dim con = contentOfdbODBC
         'Dim curODBC = runSQLwithResult("select ACCTDBSE.ODBC from MODL inner join ACCTDBSE on MODL.accountDBGUID=ACCTDBSE.accountDBGUID where MODL.ModuleId='" & code & "'")
         'Dim con = runSQLwithResult("select ODBC from CoMODL where ModuleId='" & code & "'")
-        Dim sqlstr1 = "select colname from gen.coTABLFIEL a inner join gen.coTABL b on a.tableGUID=b.TableGUID where tablename='" & ocode & "' and colorder=10"
-        Dim fieldKey As String = runSQLwithResult(sqlstr1, con)
+
+        'remarked andre
+        'Dim sqlstr1 = "select colname from gen.coTABLFIEL a inner join gen.coTABL b on a.tableGUID=b.TableGUID where tablename='" & ocode & "' and colorder=10"
+        ' Dim fieldKey As String = runSQLwithResult(sqlstr1, con)
+        Dim fieldKey As String = "docGUID"
 
         'Dim path As String = getQueryVar("path")
         'If getQueryVar("dontdelete") = 1 Then
@@ -43,7 +50,7 @@ Partial Class OPHCore_api_msg_download
 
         If fieldAttachment <> "" Then
             'Dim con = Session("odbc")
-            Dim sqlstr = "Select " & fieldAttachment & " From oph." & ocode & " Where " & fieldKey & " ='" & tGUID & "'"
+            Dim sqlstr = "Select " & fieldAttachment & " From doc." & ocode & " Where " & fieldKey & " ='" & tGUID & "'"
             Dim dt = SelectSqlSrvRows(sqlstr, con)
             path = path
             If dt.Tables(0).Rows.Count > 0 Then
