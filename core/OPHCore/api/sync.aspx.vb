16e29f663c6b16b8c5c2e1aa544bc465282f301c
Administrator <Administrator@REDBEAN>
2018-08-14 13:12:41 +0700

    --

--- core/OPHCore/api/sync.aspx.vb
+++ core/OPHCore/api/sync.aspx.vb
@@ -19,149 +19,151 @@ Partial Class OPHCore_api_sync
         If sessionToken = "" Then sessionToken = "null" Else sessionToken = "'" & sessionToken & "'"
         writeLog(mode)
         Dim isXML = True
+		if accountid<>"" then
+			Select Case mode.ToLower
+				Case "reqtoken"
+					Dim userId = getQueryVar("userId")
+					Dim pwd = getQueryVar("pwd")
+					If accountId.ToLower() <> contentOfaccountId.ToLower() Or userId = "" Or pwd = "" Then
+						result = "<sqroot><message>Wrong Parameter Input!</message></sqroot>"
+					Else
+						sqlstr = "exec api.sync_reqtoken '" & accountId & "', '" & userId & "', '" & pwd & "'"
+						xmlstr = getXML(sqlstr, contentOfsequoiaCon)
 
-        Select Case mode.ToLower
-            Case "reqtoken"
-                Dim userId = getQueryVar("userId")
-                Dim pwd = getQueryVar("pwd")
-                If accountId.ToLower() <> contentOfaccountId.ToLower() Or userId = "" Or pwd = "" Then
-                    result = "<sqroot><message>Wrong Parameter Input!</message></sqroot>"
-                Else
-                    sqlstr = "exec api.sync_reqtoken '" & accountId & "', '" & userId & "', '" & pwd & "'"
-                    xmlstr = getXML(sqlstr, contentOfsequoiaCon)
+						If xmlstr IsNot Nothing And xmlstr <> "" Then
+							'result = "<sqroot>" & xmlstr & "</sqroot>"
+							result = xmlstr
+						Else
+							result = "<sqroot><source>reqtoken</source><message>Incorrect Password!</message></sqroot>"
+						End If
 
-                    If xmlstr IsNot Nothing And xmlstr <> "" Then
-                        'result = "<sqroot>" & xmlstr & "</sqroot>"
-                        result = xmlstr
-                    Else
-                        result = "<sqroot><source>reqtoken</source><message>Incorrect Password!</message></sqroot>"
-                    End If
+					End If
+				Case "dbinfo"
+					sqlstr = "exec api.sync_dbinfo '" & accountId & "', " & sessionToken & ""
+					xmlstr = getXML(sqlstr, contentOfsequoiaCon)
 
-                End If
-            Case "dbinfo"
-                sqlstr = "exec api.sync_dbinfo '" & accountId & "', " & sessionToken & ""
-                xmlstr = getXML(sqlstr, contentOfsequoiaCon)
+					If xmlstr IsNot Nothing And xmlstr <> "" Then
+						'result = "<sqroot>" & xmlstr & "</sqroot>"
+						result = xmlstr
+					Else
+						result = "<sqroot><source>dblist</source><message>Incorrect Data!</message></sqroot>"
+					End If
+				Case "reqcorescript"
+					sqlstr = "exec core.createDB '" & accountId & "', @isScriptOnly=1, @token=" & sessionToken & ""
+					xmlstr = getXML(sqlstr, contentOfsequoiaCon, 0)
 
-                If xmlstr IsNot Nothing And xmlstr <> "" Then
-                    'result = "<sqroot>" & xmlstr & "</sqroot>"
-                    result = xmlstr
-                Else
-                    result = "<sqroot><source>dblist</source><message>Incorrect Data!</message></sqroot>"
-                End If
-            Case "reqcorescript"
-                sqlstr = "exec core.createDB '" & accountId & "', @isScriptOnly=1, @token=" & sessionToken & ""
-                xmlstr = getXML(sqlstr, contentOfsequoiaCon, 0)
+					result = xmlstr.Replace("&lt;", "<").Replace("&gt;", ">")
+					isXML = False
+					Response.ContentType = "text/plain"
+				Case "webrequestfile"
+					Dim r = download("../../ophcontent/documents/sync/", "sync.data")
+				Case "webrequestSETUP"
+					sqlstr = "exec core.webrequestsetup"
+					xmlstr = getXML(sqlstr, contentOfsequoiaCon, 0)
+				Case "checklatestevent"
+					Dim lvl = getQueryVar("lvl")
+					sqlstr = "exec [api].[sync_checklatestevent] '" & accountId & "', " & sessionToken & ", '" & lvl & "'"
 
-                result = xmlstr.Replace("&lt;", "<").Replace("&gt;", ">")
-                isXML = False
-                Response.ContentType = "text/plain"
-            Case "webrequestfile"
-                Dim r = download("../../ophcontent/documents/sync/", "sync.data")
-            Case "webrequestSETUP"
-                sqlstr = "exec core.webrequestsetup"
-                xmlstr = getXML(sqlstr, contentOfsequoiaCon, 0)
-            Case "checklatestevent"
-                Dim lvl = getQueryVar("lvl")
-                sqlstr = "exec [api].[sync_checklatestevent] '" & accountId & "', " & sessionToken & ", '" & lvl & "'"
+					xmlstr = getXML(sqlstr, contentOfdbODBC)
 
-                xmlstr = getXML(sqlstr, contentOfdbODBC)
+					If xmlstr IsNot Nothing And xmlstr <> "" Then
+						'result = "<sqroot>" & xmlstr & "</sqroot>"
+						result = xmlstr
+					Else
+						result = "<sqroot><source>checklatestevent</source><message>Incorrect Data!</message></sqroot>"
+					End If
 
-                If xmlstr IsNot Nothing And xmlstr <> "" Then
-                    'result = "<sqroot>" & xmlstr & "</sqroot>"
-                    result = xmlstr
-                Else
-                    result = "<sqroot><source>checklatestevent</source><message>Incorrect Data!</message></sqroot>"
-                End If
+				Case "reqcodeprop"
+					Dim code = getQueryVar("code")
 
-            Case "reqcodeprop"
-                Dim code = getQueryVar("code")
+					sqlstr = "exec [api].[sync_reqcodeprop] '" & accountId & "', " & sessionToken & ", '" & code & "'"
 
-                sqlstr = "exec [api].[sync_reqcodeprop] '" & accountId & "', " & sessionToken & ", '" & code & "'"
+					xmlstr = getXML(sqlstr, contentOfdbODBC)
 
-                xmlstr = getXML(sqlstr, contentOfdbODBC)
+					If xmlstr IsNot Nothing And xmlstr <> "" Then
+						'result = "<sqroot>" & xmlstr & "</sqroot>"
+						result = xmlstr
+					Else
+						result = "<sqroot><source>reqcodeprop</source><message>Incorrect Data!</message></sqroot>"
+					End If
+				Case "sendcodeprop"
+					Dim code = getQueryVar("code")
+					If Not IsNothing(Request.Form("dataXML")) Then
+						Dim dataxml = Request.Form("dataXML").ToString.Replace("%26lt;", "<").Replace("%26gt;", ">").Replace("%26", "&").Replace("&lt;", "<").Replace("&gt;", ">")
+						writeLog(Len(Request.Form("dataXML")).ToString & " " & Len(dataxml).ToString & " " + dataxml)
 
-                If xmlstr IsNot Nothing And xmlstr <> "" Then
-                    'result = "<sqroot>" & xmlstr & "</sqroot>"
-                    result = xmlstr
-                Else
-                    result = "<sqroot><source>reqcodeprop</source><message>Incorrect Data!</message></sqroot>"
-                End If
-            Case "sendcodeprop"
-                Dim code = getQueryVar("code")
-                If Not IsNothing(Request.Form("dataXML")) Then
-                    Dim dataxml = Request.Form("dataXML").ToString.Replace("%26lt;", "<").Replace("%26gt;", ">").Replace("%26", "&").Replace("&lt;", "<").Replace("&gt;", ">")
-                    writeLog(Len(Request.Form("dataXML")).ToString & " " & Len(dataxml).ToString & " " + dataxml)
+						sqlstr = "exec [api].[sync_sendcodeprop] '" & accountId & "', " & sessionToken & ", '" & code & "', '" & dataxml.Replace("'", "''") & "'"
 
-                    sqlstr = "exec [api].[sync_sendcodeprop] '" & accountId & "', " & sessionToken & ", '" & code & "', '" & dataxml.Replace("'", "''") & "'"
+						writeLog(sqlstr)
+						xmlstr = getXML(sqlstr, contentOfdbODBC)
+						writeLog(xmlstr)
+						result = "<sqroot><message>Done</message></sqroot>"
+					Else
 
-                    writeLog(sqlstr)
-                    xmlstr = getXML(sqlstr, contentOfdbODBC)
-                    writeLog(xmlstr)
-                    result = "<sqroot><message>Done</message></sqroot>"
-                Else
+						If xmlstr IsNot Nothing And xmlstr <> "" Then
+							'result = "<sqroot>" & xmlstr & "</sqroot>"
+							result = xmlstr
+						Else
+							result = "<sqroot><source>sendcodeprop</source><message>Incorrect Data!</message></sqroot>"
+						End If
+					End If
+				Case "reqheader"
+					Dim code = getQueryVar("code")
+					Dim pg = getQueryVar("page")
 
-                    If xmlstr IsNot Nothing And xmlstr <> "" Then
-                        'result = "<sqroot>" & xmlstr & "</sqroot>"
-                        result = xmlstr
-                    Else
-                        result = "<sqroot><source>sendcodeprop</source><message>Incorrect Data!</message></sqroot>"
-                    End If
-                End If
-            Case "reqheader"
-                Dim code = getQueryVar("code")
-                Dim pg = getQueryVar("page")
+					sqlstr = "exec [api].[sync_reqheader] '" & accountId & "', " & sessionToken & ", '" & code & "', " & IIf(pg = "", 1, pg) & ""
+					writeLog(sqlstr)
 
-                sqlstr = "exec [api].[sync_reqheader] '" & accountId & "', " & sessionToken & ", '" & code & "', " & IIf(pg = "", 1, pg) & ""
-                writeLog(sqlstr)
+					xmlstr = getXML(sqlstr, contentOfdbODBC)
 
-                xmlstr = getXML(sqlstr, contentOfdbODBC)
+					If xmlstr IsNot Nothing And xmlstr <> "" Then
+						'result = "<sqroot>" & xmlstr & "</sqroot>"
+						result = xmlstr
+					Else
+						result = "<sqroot><source>reqheader</source><message>Incorrect Data!</message></sqroot>"
+					End If
 
-                If xmlstr IsNot Nothing And xmlstr <> "" Then
-                    'result = "<sqroot>" & xmlstr & "</sqroot>"
-                    result = xmlstr
-                Else
-                    result = "<sqroot><source>reqheader</source><message>Incorrect Data!</message></sqroot>"
-                End If
+				Case "reqdata"
+					Dim code = getQueryVar("code")
+					Dim guid = Request.Form("guid")
+					'Dim pg = getQueryVar("page")
 
-            Case "reqdata"
-                Dim code = getQueryVar("code")
-                Dim guid = Request.Form("guid")
-                'Dim pg = getQueryVar("page")
+					sqlstr = "exec [api].[sync_reqdata] '" & accountId & "', " & sessionToken & ", '" & code & "', '" & guid & "'"
+					writeLog(sqlstr)
+					xmlstr = getXML(sqlstr, contentOfdbODBC)
+					writeLog(xmlstr)
+					If xmlstr IsNot Nothing And xmlstr <> "" Then
+						'result = "<sqroot>" & xmlstr & "</sqroot>"
+						result = xmlstr
+					Else
+						result = "<sqroot><source>reqdata</source><message>Incorrect Data!</message></sqroot>"
+					End If
 
-                sqlstr = "exec [api].[sync_reqdata] '" & accountId & "', " & sessionToken & ", '" & code & "', '" & guid & "'"
-                writeLog(sqlstr)
-                xmlstr = getXML(sqlstr, contentOfdbODBC)
-                writeLog(xmlstr)
-                If xmlstr IsNot Nothing And xmlstr <> "" Then
-                    'result = "<sqroot>" & xmlstr & "</sqroot>"
-                    result = xmlstr
-                Else
-                    result = "<sqroot><source>reqdata</source><message>Incorrect Data!</message></sqroot>"
-                End If
+				Case "senddata"
+					Dim code = getQueryVar("code")
+					writeLog(code)
+					If Not IsNothing(Request.Form("dataXML")) Then
+						Dim dataxml = Request.Form("dataXML").ToString.Replace("%26lt;", "<").Replace("%26gt;", ">").Replace("%26", "&").Replace("&lt;", "<").Replace("&gt;", ">")
+						writeLog(Len(Request.Form("dataXML")).ToString & " " & Len(dataxml).ToString & " " + dataxml)
 
-            Case "senddata"
-                Dim code = getQueryVar("code")
-                writeLog(code)
-                If Not IsNothing(Request.Form("dataXML")) Then
-                    Dim dataxml = Request.Form("dataXML").ToString.Replace("%26lt;", "<").Replace("%26gt;", ">").Replace("%26", "&").Replace("&lt;", "<").Replace("&gt;", ">")
-                    writeLog(Len(Request.Form("dataXML")).ToString & " " & Len(dataxml).ToString & " " + dataxml)
-
-                    sqlstr = "exec [api].[sync_sendData] '" & accountId & "', " & sessionToken & ", '" & code & "', '" & dataxml.Replace("'", "''") & "', 1"
-                    writeLog(sqlstr)
-                    xmlstr = getXML(sqlstr, contentOfdbODBC)
-                    writeLog(xmlstr)
-                    result = "<sqroot><message>Done</message></sqroot>"
-                Else
-                    If xmlstr IsNot Nothing And xmlstr <> "" Then
-                        'result = "<sqroot>" & xmlstr & "</sqroot>"
-                        result = xmlstr
-                    Else
-                        result = "<sqroot><source>senddata</source><message>Incorrect Data!</message></sqroot>"
-                    End If
-                End If
-            Case "reqcorescript"
-        End Select
-
+						sqlstr = "exec [api].[sync_sendData] '" & accountId & "', " & sessionToken & ", '" & code & "', '" & dataxml.Replace("'", "''") & "', 1"
+						writeLog(sqlstr)
+						xmlstr = getXML(sqlstr, contentOfdbODBC)
+						writeLog(xmlstr)
+						result = "<sqroot><message>Done</message></sqroot>"
+					Else
+						If xmlstr IsNot Nothing And xmlstr <> "" Then
+							'result = "<sqroot>" & xmlstr & "</sqroot>"
+							result = xmlstr
+						Else
+							result = "<sqroot><source>senddata</source><message>Incorrect Data!</message></sqroot>"
+						End If
+					End If
+				Case "reqcorescript"
+			End Select
+		else
+			result="account is not exists"
+		end if
         If isXML Then
             Response.ContentType = "text/xml"
             Response.Write("<?xml version=""1.0"" encoding=""utf-8""?>")
