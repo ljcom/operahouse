c96e4f6ca08e2c6d5bda0bcc79084c744b9e3053
Samuel <samuel@libertyjayaone.local>
2018-12-06 08:32:46 +0700

    --

--- core/_health.aspx.vb
+++ core/_health.aspx.vb
@@ -5,12 +5,21 @@ Partial Class _health
     Protected Sub Page_Load(sender As Object, e As System.EventArgs) Handles Me.Load
         Dim appSettings As NameValueCollection = ConfigurationManager.AppSettings
         Dim contentOfsequoiaCon = appSettings.Item("sequoia")
-
-        Dim sqlstr = "select status from node where nodename='" & My.Computer.Name.ToString & "'"
+        Dim ip = GetIPAddress(My.Computer.Name.ToString)
+        Dim sqlstr = "select status from node where nodeIP='" & ip.ToString & "'"
         result = runSQLwithResult(sqlstr, contentOfsequoiaCon).ToString
         If result <> "400" Then
             sqlstr = "update node set status=400 where status<>500 and nodename='" & My.Computer.Name.ToString & "'"
             runSQL(sqlstr, contentOfsequoiaCon)
         End If
     End Sub
+    Private Function GetIPAddress(strHostName) As String
+
+        Dim strIPAddress As String
+
+        'strHostName = System.Net.Dns.GetHostName()
+        strIPAddress = Net.Dns.GetHostByName(strHostName).AddressList(0).ToString()
+        Return strIPAddress
+
+    End Function
 End Class
